1f83355e122a8d78f2d810848a62972493769d6a
Samuel Felipe <samfelgar@gmail.com>
2024-04-02 02:14:09 -0300

    Create PixCharge webhook payload

---
+++ src/PixCharges/Models/PixChargeWebhookPayload.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Samfelgar\Inter\PixCharges\Models;
+
+use Psr\Http\Message\ResponseInterface;
+use Samfelgar\Inter\Common\ResponseUtils;
+
+readonly class PixChargeWebhookPayload
+{
+    public function __construct(
+        public string $chargeId,
+        public string $yourNumber,
+        public Situation $situation,
+        public \DateTimeImmutable $situationDate,
+        public float $receivedAmount,
+        public ChargeReceivementOrigin $origin,
+        public string $ourNumber,
+        public string $barCode,
+        public string $line,
+        public string $pixId,
+        public string $copyAndPasteCode,
+    ) {
+    }
+
+    public static function fromArray(array $data): PixChargeWebhookPayload
+    {
+        return new PixChargeWebhookPayload(
+            $data['codigoSolicitacao'],
+            $data['seuNumero'],
+            Situation::from($data['situacao']),
+            \DateTimeImmutable::createFromFormat(\DateTimeInterface::RFC3339, $data['dataHoraSituacao']),
+            (float) $data['valorTotalRecebido'],
+            ChargeReceivementOrigin::from($data['origemRecebimento']),
+            $data['nossoNumero'],
+            $data['codigoBarras'],
+            $data['linhaDigitavel'],
+            $data['txid'],
+            $data['pixCopiaECola'],
+        );
+    }
+
+    /**
+     * @return PixChargeWebhookPayload|PixChargeWebhookPayload[]
+     */
+    public static function fromResponse(ResponseInterface $response): PixChargeWebhookPayload|array
+    {
+        $data = ResponseUtils::responseToArray($response);
+        if (\array_is_list($data)) {
+            return \array_map(self::fromArray(...), $data);
+        }
+        return self::fromArray($data);
+    }
+}
